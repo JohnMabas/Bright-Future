cfab233ffa129f8d49d423c5b73cb1dbe46031ae
mabas <johnmabas1@gmail.com>
2026-09-02 16:51:57 +0100

    add WhyChoose component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Hero from './components/Hero'
 import NavBar from './components/NavBar'
 import Stats from './components/Stats'
+import WhyChoose from './components/WhyChoose'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <NavBar/>
       <Hero/>
       <Stats/>
+      <WhyChoose/>
     </>
   )
 }
---
+++ src/components/WhyChoose.jsx
@@ -0,0 +1,93 @@
+import { FaPeopleGroup } from "react-icons/fa6";
+import StudentHero from "/public/image/student -hero.png"
+
+
+
+export default function WhyChoose() {
+  return (
+    <div>
+      <div className="p-10">
+        <h2 className="text-[#0F3158] text-3xl font-bold">Why Choose BrightFuture</h2>
+        <p className="text-[#0F3158] text-sm">Hightlighting what makes the school unique</p>
+        <div className="flex gap-5">
+            <div className="flex items-center gap-2">
+
+            <div className="space-y-5">
+                <div className="flex gap-3 items-center">
+                    <div className="rounded-full p-3 w-10  bg-[#0F3158]">
+                        <FaPeopleGroup className="w-4"/>
+                    </div>
+                    <span>
+                        <b className="text-[#0F3158]">Expericened Teachers</b>
+                        <p className="text-[#0F3158] text-sm">Passionate and qualified educators</p>
+                    </span>
+                </div>
+
+                 <div className="flex gap-3 items-center">
+                    <div className="rounded-full p-3 w-10  bg-[#0F3158]">
+                        <FaPeopleGroup className="w-4"/>
+                    </div>
+                    <span>
+                        <b className="text-[#0F3158]">Modern Facilies</b>
+                        <p className="text-[#0F3158] text-sm">State-of-the-art learning spaces</p>
+                    </span>
+                </div>
+
+                 <div className="flex gap-3 items-center">
+                    <div className="rounded-full p-3 w-10  bg-[#0F3158]">
+                        <FaPeopleGroup className="w-4"/>
+                    </div>
+                    <span>
+                        <b className="text-[#0F3158]">Sports & Extracurricular</b>
+                        <p className="text-[#0F3158] text-sm">Build skills, confidence and teamwork</p>
+                    </span>
+                </div>
+
+            </div>
+
+
+            <div>
+                <div className="space-y-5">
+                <div className="flex gap-3 items-center">
+                    <div className="rounded-full p-3 w-10  bg-[#0F3158]">
+                        <FaPeopleGroup className="w-4"/>
+                    </div>
+                    <span>
+                        <b className="text-[#0F3158]">Expericened Teachers</b>
+                        <p className="text-[#0F3158] text-sm">Passionate and qualified educators</p>
+                    </span>
+                </div>
+
+                 <div className="flex gap-3 items-center">
+                    <div className="rounded-full p-3 w-10  bg-[#0F3158]">
+                        <FaPeopleGroup className="w-4"/>
+                    </div>
+                    <span>
+                        <b className="text-[#0F3158]">Modern Facilies</b>
+                        <p className="text-[#0F3158] text-sm">State-of-the-art learning spaces</p>
+                    </span>
+                </div>
+
+                 <div className="flex gap-3 items-center">
+                    <div className="rounded-full p-3 w-10  bg-[#0F3158]">
+                        <FaPeopleGroup className="w-4"/>
+                    </div>
+                    <span>
+                        <b className="text-[#0F3158]">Sports & Extracurricular</b>
+                        <p className="text-[#0F3158] text-sm">Build skills, confidence and teamwork</p>
+                    </span>
+                </div>
+
+            </div>
+            </div>
+
+            </div>
+
+            <div>
+                <img src={StudentHero} alt="" className="w-125" />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
